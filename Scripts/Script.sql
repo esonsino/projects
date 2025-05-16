01f03c80c32abfd9296dd77e10b8f54cd76922c0
esonsino <eytan.sonsino@gmail.com>
2025-05-16 18:32:13 +0200

    init

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -1,2 +1,26 @@
 -- netprice * quantity * exchangerate
 
+WITH ltv AS (
+	SELECT
+		customerkey,
+		SUM(netprice * quantity * exchangerate)::integer AS lifetime_value
+	FROM sales s 
+	GROUP BY customerkey 
+	ORDER BY customerkey 
+),
+percentiles AS (
+SELECT
+	PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY lifetime_value) AS percentile_25th,
+	PERCENTILE_CONT(0.50) WITHIN GROUP (ORDER BY lifetime_value) AS percentile_50th,
+	PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY lifetime_value) AS percentile_75th
+FROM ltv
+)
+SELECT
+	customerkey,
+	lifetime_value,
+	CASE
+		WHEN lifetime_value < percentile_25th THEN '1 - Low Value'
+		WHEN lifetime_value BETWEEN percentile_25th AND percentile_50th THEN '2 - Mid Value'
+		ELSE '3 - High Value'
+	END
+FROM ltv, percentiles
